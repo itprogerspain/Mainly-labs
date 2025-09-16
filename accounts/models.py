fb072afc5f3c66e6275deb64467e577d66011ee8
itprogerspain <itprogerspain@gmail.com>
2025-09-17 00:16:55 +0200

    Reorganized templates structure (moved profile_form to profile_edit)

--- accounts/models.py
+++ accounts/models.py
@@ -13,32 +13,3 @@ class CustomManagerUser(UserManager):
 
 
 # Extended user model
-class CustomUser(AbstractUser):
-    ROLE_CHOICES = [
-        ('superadmin', 'Super Admin'),
-        ('admin', 'Admin'),
-        ('user', 'User'),
-    ]
-
-    role = models.CharField(
-        max_length=20,
-        choices=ROLE_CHOICES,
-        default='user'
-    )
-
-    email = models.EmailField(max_length=100, unique=True, blank=False)
-    phone_number = models.CharField(max_length=20, null=True, blank=True)
-
-    avatar = models.ImageField(
-        upload_to="profiles/",
-        blank=True,
-        null=True,
-        default="profiles/default.jpg"
-    )
-
-    def __str__(self):
-        return self.username
-
-    objects = CustomManagerUser()
-
-
