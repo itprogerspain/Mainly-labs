38bf76ce12862076346154ad3f7cc182a53590b5
itprogerspain <itprogerspain@gmail.com>
2025-09-17 03:04:19 +0200

    Bug fixing2

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
+
+
+# Custom manager to handle user and superuser creation
 class CustomManagerUser(UserManager):
     def create_user(self, username, email, password, **extra_fields):
         return super().create_user(username, email, password, **extra_fields)
